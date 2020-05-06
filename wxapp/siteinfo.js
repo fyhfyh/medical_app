28cd03357de2fc158ca19617577ff78ee4f4f50b
fyh <1064867470@qq.com>
2020-05-06 10:43:19 +0800

    first commit

---
+++ wxapp/siteinfo.js
@@ -0,0 +1,12 @@
+var siteinfo = {
+    name: "医疗小程序",
+    uniacid: "2",
+    acid: "2",
+    multiid: "0",
+    version: "5.9.7",
+  siteroot: "http://www.yl.com/app/index.php",
+    // siteroot: "http://106.52.74.174/app/index.php",
+    design_method: "3"
+};
+
+module.exports = siteinfo;
